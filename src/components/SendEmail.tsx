c0abf503252506f183905d98b52dc629fe18a750
Seunggeun Yoon <tmdmrs522@gmail.com>
2025-05-27 13:31:58 +0900

    resolve

--- src/components/SendEmail.tsx
+++ src/components/SendEmail.tsx
@@ -1,26 +1,26 @@
-'use client';
+// 'use client';
 
-export default function SendEmail() {
-  const sendEmails = async () => {
-    const res = await fetch('/api/send-bulk-email', {
-      method: 'POST',
-    });
+// export default function SendEmail() {
+//   const sendEmails = async () => {
+//     const res = await fetch('/api/send-bulk-email', {
+//       method: 'POST',
+//     });
 
-    if (res.ok) {
-      alert('이메일 전송 완료!');
-    } else {
-      alert('전송 실패 😢');
-    }
-  };
+//     if (res.ok) {
+//       alert('이메일 전송 완료!');
+//     } else {
+//       alert('전송 실패 😢');
+//     }
+//   };
 
-  return (
-    <div className='p-10'>
-      <button
-        onClick={sendEmails}
-        className='px-6 py-3 bg-blue-600 text-white rounded hover:bg-blue-700'
-      >
-        전체 회원에게 이메일 보내기
-      </button>
-    </div>
-  );
-}
+//   return (
+//     <div className='p-10'>
+//       <button
+//         onClick={sendEmails}
+//         className='px-6 py-3 bg-blue-600 text-white rounded hover:bg-blue-700'
+//       >
+//         전체 회원에게 이메일 보내기
+//       </button>
+//     </div>
+//   );
+// }
